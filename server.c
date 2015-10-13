5e30db488bf052483882abdabd56c451344a8b4f
jeremy <jeremymartinez11@gmail.com>
2015-10-12 18:15:13 -0700

    server welcome added

--- server.c
+++ server.c
@@ -32,18 +32,23 @@ int main(int argc, char *argv[])
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(portno);
     if (bind(sockfd, (struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
-             error("ERROR on binding");
+        error("ERROR on binding");
     listen(sockfd,5);
     clilen = sizeof(cli_addr);
+
+    int has_connected = false;
+
     while(1) {
         newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
         if (newsockfd < 0) 
-             error("ERROR on accept");
+            error("ERROR on accept");
         bzero(buffer,256);
         n = read(newsockfd,buffer,255);
         if (n < 0) error("ERROR reading from socket");
         printf("strcpy: %d",strcmp(buffer,credentials));
         printf("Here is the message: %s\n",buffer);
+        if (has_connected == false)
+            n = write(newsockfd,"welcome\n",8);
         n = write(newsockfd,"I got your message",18);
         if (n < 0) error("ERROR writing to socket");
     }
